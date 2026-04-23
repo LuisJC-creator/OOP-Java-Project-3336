9b224a75e22bd280930945c954e10cb85b3e953f
Luis Chavez <luis.chavez04@utrgv.edu>
2026-04-23 00:35:56 +0000

    added some init Board fields and fn signatures

--- src/main/Board.java
+++ src/main/Board.java
@@ -1,3 +1,30 @@
 public class Board {
-    // TODO
+    
+    private int width;
+    private int length;
+
+    // the "board"
+    private Entity board[][];
+
+    public Board(int w, int l){
+        this.width = w;
+        this.length = l;
+        this.board = new Entity[length][width];
+    }
+
+
+    // so game can call this on initialization, easy peasy.
+    public void placeEntities(Entity e, int x, int y){
+
+    }
+
+    // redundancy?
+    public void moveEntity(Entity e, int x, int y){
+
+    }
+
+    // check if position is within board bounds.
+    public boolean isValidPosition(int x, int y){
+
+    }
 }
